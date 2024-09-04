7a5a639b1fb8eb3c78718bc84442b2376604b191
Juan-Espino <juanespino@protonmail.com>
2024-09-04 15:00:43 -0700

    FINALLY FOUND HOW TO DO THE CORRECT ANIMATION!!!

--- src/carousel/CarouselItem.tsx
+++ src/carousel/CarouselItem.tsx
@@ -28,13 +28,18 @@ const CarouselItem = ({
 		// 		></div>
 		// 	</div>
 		// </section>
-		<section id="icon-block" className={`${styles.circleItem} `}>
+		<section
+			id="icon-block"
+			// className={`${styles.circleItem}  animate-around-the-world ${delayTiming}`}
+
+			className={`${styles.circleItem} absolute left-[calc(50%-6.25rem)] h-[25rem] w-[12.5rem] top-[6.5rem] `}
+		>
 			<img src={img} className="" />
 			<div id="reflection" className="relative ">
-				<img src={img} className="-scale-y-[1] opacity-20  " />
+				<img src={img} className="-scale-y-[1] opacity-20" />
 				<div
 					id="overlay"
-					className="absolute top-0 left-0 size-64 bg-gradient-to-t from-white from-20%	"
+					className="absolute top-0 left-0 h-[12.5rem] w-[12.5rem] bg-gradient-to-t from-white from-20%	"
 				></div>
 			</div>
 		</section>
--- src/carousel/CarouselWrapper.tsx
+++ src/carousel/CarouselWrapper.tsx
@@ -5,14 +5,19 @@ import styles from "./CarouselWrapper.module.css";
 
 const CarouselWrapper = () => {
 	return (
-		<ul id="carousel" className={`${styles.circle} w-full h-full mx-auto`}>
+		<ul id="carousel" className={`${styles.circle} w-full h-full mx-auto `}>
 			{/* todo:map every item [img,delay*arbitray value] */}
 
-			<CarouselItem img={img} />
-			<CarouselItem img={img} delay="-animation-delay-5000" />
+			<li className={`${styles.listItem} relative`}>
+				<CarouselItem img={img} />
+			</li>
+			{/* <li className={`${styles.listItem} relative`}>
+				<CarouselItem img={img} delay="-animation-delay-10000" />
+			</li> */}
+			{/* 
 
 			<CarouselItem img={img} delay="-animation-delay-10000" />
-			<CarouselItem img={img} delay="-animation-delay-15000" />
+			<CarouselItem img={img} delay="-animation-delay-15000" /> */}
 		</ul>
 	);
 };
